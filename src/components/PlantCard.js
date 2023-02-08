f4d86330d55b2735a6a04ddd16a067c7621fbf09
iandanielwells <114944387+iandanielwells@users.noreply.github.com>
2023-02-08 14:53:02 -0500

    Update PlantCard.js

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
-function PlantCard() {
+function PlantCard( {image, name, price} ) {
+
+  const [inStock, setInStock] = useState(true)
+
+  //if this function does not work, then have to create two separate functions for handling both in and out of stock
+  function handleClick(event) {
+    setInStock(prev => !prev)
+  }
+
   return (
     <li className="card">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      <img src={image} alt={name} />
+      <h4>{name}</h4>
+      <p>Price: {price}</p>
+      {inStock ? (
+        <button onClick={handleClick} className="primary">In Stock</button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick={handleClick}>Out of Stock</button>
       )}
     </li>
   );
