6bcb1b776bda7cd4c0be76892f3434497fc879f3
iandanielwells <114944387+iandanielwells@users.noreply.github.com>
2023-02-08 14:53:49 -0500

    Update PlantPage.js

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+
+  const [list, setList] = useState([])
+  const [search, setSearch] = useState("")
+
+
+  useEffect(() => {
+    fetch('http://localhost:6001/plants')
+      .then(resp => resp.json())
+      .then(data => setList(data))
+  }, [])
+
+  function addPlant(newPlant) {
+    setList(prev => [...prev, newPlant])
+  }
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm
+        onAddPlant={addPlant} />
+      <Search 
+        search={search}
+        setSearch={setSearch} />
+      <PlantList
+        list={list}
+        setList={setList}
+        search={search} />
     </main>
   );
 }
