d45f429998bf562c5f0b379a2bf38037e677f75e
iandanielwells <114944387+iandanielwells@users.noreply.github.com>
2023-02-08 14:53:24 -0500

    Update PlantList.js

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,24 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList( {list, search} ) {
+
+  const filteredList = list.filter(plant => {
+    return plant.name.toLowerCase().includes(search.toLowerCase())
+  })
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">
+      {filteredList.map(plant => {
+        return <PlantCard
+        key={plant.id}
+        name={plant.name}
+        image={plant.image}
+        price={plant.price}
+        />
+      })}
+      
+      </ul>
   );
 }
 
