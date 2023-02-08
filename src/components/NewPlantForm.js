4be771804d104fc12c5758d10fe9ae8aec75ca9d
iandanielwells <114944387+iandanielwells@users.noreply.github.com>
2023-02-08 14:52:37 -0500

    Update NewPlantForm.js

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,8 +1,33 @@
 import React from "react";
 
-function NewPlantForm() {
+function NewPlantForm({onAddPlant}) {
+
+  function handleSubmit(event) {
+    event.preventDefault()
+
+    const newPlant = {
+      image: event.target.image.value,
+      name: event.target.name.value,
+      price: event.target.price.value
+    }
+
+    fetch('http://localhost:6001/plants', {
+      method: 'POST',
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify(newPlant)
+    })
+      .then(onAddPlant(newPlant))
+
+      event.target.reset()
+  }
+
+  
   return (
-    <div className="new-plant-form">
+    <div 
+      onSubmit={handleSubmit}
+      className="new-plant-form">
       <h2>New Plant</h2>
       <form>
         <input type="text" name="name" placeholder="Plant name" />
